d3e89680d1b4cb912766d4acb6cbac5b7834539a
Daniel Akinniranye <danielakinniranye@Daniels-MacBook-Pro.local>
2022-06-18 13:19:39 -0400

    Added a gitignore file to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000; //problem connecting to server
+
 app.use(cors());
 app.use(express.json());
 // app.use(require("./routes/record"));
