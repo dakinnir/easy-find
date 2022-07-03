dec1451a28dd20d64b747804ba4457657a7544e0
Daniel Akinniranye <danielakinniranye@Daniels-MacBook-Pro.local>
2022-07-03 09:02:34 -0400

    Completed the user settings page

---
+++ client/src/settings/components/SidebarOptions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/SidebarOptions.css'
+
+export default function SidebarOptions() {
+  return (
+    <div className="option-container">
+        <div className="options">
+            <a className="option" href="#">User Settings</a>
+            <a className="option" href="#">Work History</a>
+        </div>
+    </div>
+  )
+}
